a2e06803a89cd3264c0d64ec247cdfb8e863b1c8
Abhishek Sharma <sharmaabhi260@gmail.com>
2021-01-11 12:36:27 +0530

    update

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm#igs@(7^jjq2nw-$%cm+zlvr5fr@d$dmd4l5fkm)$8sol_1or'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://djangoapp-env.eba-r5remfjp.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['https://djangoapp-env.eba-r5remfjp.us-east-1.elasticbeanstalk.com/']
 
 
 # Application definition
