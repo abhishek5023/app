58d620875b5f199a779203c06994e2f24291fb28
Abhishek Sharma <sharmaabhi260@gmail.com>
2021-01-11 12:55:32 +0530

    update

--- app/wsgi.py
+++ app/wsgi.py
@@ -7,11 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import sys, os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, '/root/Desktop/fexle/Django/app')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
